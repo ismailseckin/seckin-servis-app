2b0419e5b29ab0ff490f170c0a07f810784e81b8
ismailseckin <ismailseckin04@gmail.com>
2026-03-21 01:11:42 +0300

    fix: PWA manifest/icon/sw yollarini gorecel yap

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-const CACHE = 'seckin-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'seckin-v2';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -19,6 +24,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
